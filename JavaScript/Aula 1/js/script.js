542a7d29595f7e8575f62493fb4846ce76bdb7ea
jooj08 <joao.arbues@estudante.ifto.edu.br>
2026-05-26 22:01:28 -0300

    Add files via upload

---
+++ JavaScript/Aula 1/js/script.js
@@ -0,0 +1,19 @@
+  document.getElementById("demo").innerHTML = "Pedrão";
+
+    function aumentar(){
+        document.getElementById("demo").style.fontSize = "35px";
+    }
+
+    function diminuir(){
+        document.getElementById("demo").style.fontSize = "15px"
+    }
+
+    function desligar(){
+        document.getElementById("lampada").src = "img/image.png"
+
+    }
+
+    function ligar(){
+        document.getElementById("lampada").src = "img/WhatsApp Image 2026-05-26 at 20.09.17.jpeg"
+        
+    }
---
+++ JavaScript/Aula 5/js/script.js
@@ -0,0 +1,7 @@
+document.getElementById("btnMulher").addEventListener("click", function(){
+    document.getElementById("header").style.backgroundColor = "blueviolet";
+}, false)
+
+document.getElementById("btnHomem").addEventListener("click", function(){
+    document.getElementById("header").style.backgroundColor = "blue";
+}, false)
